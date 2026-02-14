d7a686c0ad1d6dc3ff2dfbaf3d9e04f09b3e7bf9
Boqiang <zhan4329@purdue.edu>
2026-02-14 17:17:28 -0500

    work: question 5

---
+++ app.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+import torch
+import pickle
+
+from src.neural_net import SimpleNN
+
+app = FastAPI()
+
+# loading the model up
+input_size = 4 # Iris dataset has 4 features
+num_classes = 3
+model = SimpleNN(input_size, num_classes) # recreating the model architecture!
+model.load_state_dict(torch.load("models/best_model.pth"))
+model.eval()
+
+with open("models/label_encoder.pkl", "rb") as f:
+    label_encoder = pickle.load(f)
+
+@app.post("/predict")
+def predict(data: list[float]):
+    features = torch.FloatTensor(data).unsqueeze(0)
+
+    with torch.no_grad():
+        outputs = model(features)
+        _, predicted_idx = torch.max(outputs, 1)
+        predicted_label = label_encoder.inverse_transform(predicted_idx.numpy())[0] # decodes the encoded label
+
+    return {"prediction": predicted_label}
