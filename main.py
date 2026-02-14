96395d4c49af4bcf5ad2e0ed582ca1adaf35c16d
Boqiang <zhan4329@purdue.edu>
2026-02-13 20:30:22 -0500

    work(proj4): initial commit for question 1
    - run main.py
    - reorganize directories

--- main.py
+++ main.py
@@ -22,9 +22,10 @@ Normally we would put these in a gloabl file and import them but we will not do 
 """
 # Once you get root_directory_path set to point at the root of the working dir, you should NOT change it
 # only change the relative paths like data_path, model_path etc
-root_directory_path = Path("~/project-04/intro-mlops-1/").expanduser()
+project_dir = "~/seminar-project/project4"
+root_directory_path = Path(f"{project_dir}/intro-mlops-1/").expanduser()
 
-data_path =  root_directory_path / "data.csv" # NOTE: make sure to update the path when you create the new directories i.e / "data" / "data.csv"
+data_path =  root_directory_path / "data" / "data.csv" # NOTE: make sure to update the path when you create the new directories i.e / "data" / "data.csv"
 
 print("Loading dataset...")
 data = pd.read_csv(data_path)
